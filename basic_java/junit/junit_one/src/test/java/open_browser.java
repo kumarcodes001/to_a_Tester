10c51c3baf8eeb48dede2c588a9f3174d2f867c1
Kumarcodes001 <arunkumarpalani001@gmail.com>
2025-12-31 12:47:14 +0530

    junit_browser_basics

--- basic_java/junit/junit_one/src/test/java/open_browser.java
+++ basic_java/junit/junit_one/src/test/java/open_browser.java
@@ -1,6 +1,8 @@
+import org.junit.After;
 import org.junit.Before;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,14 +11,29 @@ public class open_browser {
     WebDriver driver;
 
 
-    @Test
+    @Before
     public void open_browser(){
         String driverPath = "F:\\Testing\\Junit\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/");
+    }
 
 
+    @Test
+    public void getTitle(){
+       String title=  driver.getTitle();
+       System.out.println("Title : " + title);
+
+       // File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+       // FileUtils.copyFile(src, new File("screenshot.png"));
 
     }
+
+    @After
+    public void close_Browser() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.close();
+    }
+
 }
