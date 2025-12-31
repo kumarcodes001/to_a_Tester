c269d565e6aae206989adea4ea014fbb710c47a7
Kumarcodes001 <arunkumarpalani001@gmail.com>
2025-12-31 13:00:06 +0530

    space

--- basic_java/junit/junit_one/src/test/java/junit_setup.java
+++ basic_java/junit/junit_one/src/test/java/junit_setup.java
@@ -1,22 +1,15 @@
-
 import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class junit_setup {
 
     String message = "BrowserStack is the intended message" ;
-
-
     @Test
-
     public void testMessage() {
-
         System.out.println("Inside testMessage()");
-
         assertEquals("BrowserStack is the intended message", message);
     }
-    }
+}
 
