c35fb6e4e25aec5c01f115645084617c18b5d9fe
Kumarcodes001 <arunkumarpalani001@gmail.com>
2026-01-07 19:01:01 +0530

    implicity wait and table xpath

--- basic_java/junit/junit_one/src/test/java/table_operations.java
+++ basic_java/junit/junit_one/src/test/java/table_operations.java
@@ -1,7 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class table_operations {
     WebDriver driver;
@@ -12,10 +17,19 @@ public class table_operations {
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
         driver.get("https://www.w3schools.com/html/html_tables.asp");
+
+        //implicity wait
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
     }
 
     @Test
     public void table_ops(){
-        
+
+        //Find the xpath of the coloumn value which have alphapet "e" and "i"
+        List<WebElement> Ele = driver.findElements(By.xpath("//table[@class='ws-table-all']/tbody/tr/td[2][contains(text(),'e') and contains(text(),'i')]"));
+        //print the elements
+        for(WebElement word : Ele){
+            System.out.println("Word ===>" + word.getText());
+        }
     }
 }
