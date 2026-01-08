b5b5de4f95db538ba693e7941b8bda81a8fb5e39
Kumarcodes001 <arunkumarpalani001@gmail.com>
2026-01-08 14:13:47 +0530

    testNG creation

---
+++ testNG/testNG_one/src/test/java/FirstTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void welcomeTest() {
+        System.out.println("Hello! Your TestNG project is set up correctly.");
+    }
+}
