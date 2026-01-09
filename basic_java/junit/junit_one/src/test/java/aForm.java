cd3c501c8ce227765a4691a499295e3660ae4b79
Kumarcodes001 <arunkumarpalani001@gmail.com>
2026-01-09 19:46:53 +0530

    form_over_code_crash

--- basic_java/junit/junit_one/src/test/java/aForm.java
+++ basic_java/junit/junit_one/src/test/java/aForm.java
@@ -3,8 +3,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class aForm {
@@ -19,11 +22,17 @@ public class aForm {
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void forma_fillup() throws InterruptedException {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement success = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("text-center")));
+        System.out.println("Page opened ->>" + success);
+
 
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,500);");
@@ -52,18 +61,37 @@ public class aForm {
         WebElement date = driver.findElement(By.xpath("//div[@class = 'react-datepicker__day react-datepicker__day--004']"));
         date.click();
 
-        js.executeScript("window.scrollBy(0,300);");
+        js.executeScript("window.scrollBy(0,100);");
 
         //DropDown
-        WebElement subject_dd = driver.findElement(By.xpath("//*[@id=\"subjectsContainer\"]/div/div[1]"));
-        subject_dd.click();
-        subject_dd.sendKeys("English");
+        /*WebElement subject_dd = driver.findElement(By.id("subjectsContainer"));
+       // subject_dd.click();
+       // Thread.sleep(1000);
+        driver.findElement(By.xpath("")).sendKeys("Engl");
         subject_dd.sendKeys(Keys.ENTER);
+*/
+
+        //checkBox
+     //  WebElement hobbies = driver.findElement(By.id("hobbies-checkbox-1"));
+    //   hobbies.click();
+
+        //Upload a file -
+        WebElement up_element = driver.findElement(By.id("uploadPicture"));
+        up_element.sendKeys("F:/Testing/File.txt");
 
 
+        //dropdown funtionalities
 
+        /*WebElement dd_state = driver.findElement(By.className("css-yk16xz-control"));
+        Select dd_ele = new Select(dd_state);
+        dd_ele.selectByIndex(1);
 
+        WebElement dd_div = driver.findElement(By.className("css-1wa3eu0-placeholder"));
+        Select dd_div_ele = new Select(dd_div);
+        dd_div_ele.selectByIndex(2);*/
 
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
 
 
 
@@ -77,7 +105,7 @@ public class aForm {
 
     @After
     public void close_browser() throws InterruptedException {
-        Thread.sleep(1000);
-        driver.close();
+        //Thread.sleep(1000);
+        driver.quit();
     }
 }
